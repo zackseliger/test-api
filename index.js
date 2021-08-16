690c44807787dd25b1ac01d920b7a2ed1c9be041
Zack Seliger <zseliger@gmail.com>
2021-08-15 23:07:42 -0500

    initial program

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/random', function(req, res) {
+    res.send(Math.floor(Math.random()*10000));
+});
+
+app.listen(process.env.PORT || 80, function(err) {
+    if (err) return console.log(err);
+
+    console.log("listening on port"+(process.env.PORT || 80));
+});
