e90255614c8abaff5fd6306750c51e87dd204aff
Zack Seliger <zseliger@gmail.com>
2021-08-17 13:13:32 -0500

    output fix, docker and ECS update

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/random', function(req, res) {
-    res.send(Math.floor(Math.random()*10000));
+    res.send(""+Math.floor(Math.random()*10000));
 });
 
 app.listen(process.env.PORT || 80, function(err) {
     if (err) return console.log(err);
 
-    console.log("listening on port"+(process.env.PORT || 80));
+    console.log("listening on port "+(process.env.PORT || 80));
 });
